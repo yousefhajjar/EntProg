493d09629360f9428e1af4f7a3ad618d30f03c69
yousefhajjar <yousef.hajjar.e22058@mcast.edu.mt>
2022-11-16 21:35:58 +0100

    lesson 1.

---
+++ DataAccess/Repositories/ItemRepositories.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    internal class ItemRepositories
+    {
+        public IQueryable<Item> GetItems()
+        { }
+
+        public void AddItem()
+        {
+
+
+        }
+    }
+}
